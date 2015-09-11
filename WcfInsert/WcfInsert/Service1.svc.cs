b4cc271dac5351fba59026fd124a35ea1bb08a87
unknown <ouvashisht.banka@MR2-DTP08-0R3C.dir.svc.accenture.com>
2015-09-11 10:14:53 +0400

    inse

--- WcfInsert/WcfInsert/Service1.svc.cs
+++ WcfInsert/WcfInsert/Service1.svc.cs
@@ -26,7 +26,7 @@ namespace WcfInsert
 
             if (result == 1)
             {
-                message = UserInfo.UserName + "Details i";
+                message = UserInfo.UserName + "Details inse";
 
             }
 
