be8449529d10f082d29c4880ee676210d8abc0e9
unknown <ouvashisht.banka@MR2-DTP08-0R3C.dir.svc.accenture.com>
2015-09-21 09:49:17 +0400

    xxx

--- WcfInsert/WcfInsert/Service1.svc.cs
+++ WcfInsert/WcfInsert/Service1.svc.cs
@@ -26,7 +26,7 @@ namespace WcfInsert
 
             if (result == 1)
             {
-                message = UserInfo.UserName + "Details inserted sucess";
+                message = UserInfo.UserName + "Details inserted sucessfully";
 
             }
 
